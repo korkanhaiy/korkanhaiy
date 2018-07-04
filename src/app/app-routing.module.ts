235e7ed4444bfc4479f45358a962140822fcea75
KouiDeveloper <kouiprogramer@gmail.com>
2018-07-04 12:09:42 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { A002IndexComponent } from './a002-index/a002-index.component';
 import { A003LoginComponent } from './a003-login/a003-login.component';
 import { A004RegisterComponent } from './a004-register/a004-register.component';
+import { A005AllTeacherPageComponent } from './a005-all-teacher-page/a005-all-teacher-page.component';
 
 const routes: Routes = [
   { path: '', component: A002IndexComponent},
   { path: 'index', component:A002IndexComponent},
   { path: 'login', component:A003LoginComponent},
-  { path: 'register', component:A004RegisterComponent}
+  { path: 'register', component:A004RegisterComponent},
+  { path: 'all-techer', component:A005AllTeacherPageComponent}
 ]
 @NgModule({
   imports: [
